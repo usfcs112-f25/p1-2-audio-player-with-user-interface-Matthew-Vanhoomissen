73acf04671f1673a6b65f968a73c77e0af5acef8
Matthew-Vanhoomissen <ma.vanhoom1@gmail.com>
2025-09-20 20:40:50 -0700

    2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,6 +10,12 @@ public class Main {
         testing.addSong(new Song("Test2", "T", 10, "T", "///"));
 
         JFrame frame = new JFrame("Spotify Playlist (Swing)");
+
+        JTextField input = new JTextField(15);
+        frame.setLayout(new BorderLayout());
+        JPanel test = new JPanel();
+        test.add(input);
+        frame.add(test, BorderLayout.NORTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 400);
         frame.setVisible(true);
